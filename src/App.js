eb06d8747cc300eb5b68a3f1b39fb4979b3cf943
jeewantha upul <59133288+jeewantha-upul@users.noreply.github.com>
2022-12-14 12:34:46 +0530

    sidebar basic skeleton developed

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ function App() {
             }
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
-              NavBar
+              <Navbar />
             </div>
 
             {/* ------  Routing part */}
