99ab37d479017aaa388a27299faa0f46263eb732
jeewantha upul <59133288+jeewantha-upul@users.noreply.github.com>
2022-12-15 09:38:03 +0530

    navbar with its components developed

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -2,13 +2,34 @@ import React, { createContext, useContext, useState } from "react";
 
 const StateContext = createContext();
 
+// initial states relavant to navbar components
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
+
 export const ContextProvider = ({ children }) => {
   //all states are mentioned here
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+
+  // to check the clicked component in the navbar
+  const handleClick = (clicked) =>
+    setIsClicked({ ...initialState, [clicked]: true });
 
   return (
     // this value property sends the required state values to other components
-    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+      }}
+    >
       {children}
     </StateContext.Provider>
   );
